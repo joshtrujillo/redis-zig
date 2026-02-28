034f1f7497b4693ad4154b4dfc30f8c34a21297c
Josh Trujillo <josh.trujillo0@pm.me>
2026-02-27 21:16:24 -0700

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -8,17 +8,17 @@ pub fn main() !void {
 
     // Uncomment the code below to pass the first stage
     //
-    // const address = try net.Address.resolveIp("127.0.0.1", 6379);
-    //
-    // var listener = try address.listen(.{
-    //     .reuse_address = true,
-    // });
-    // defer listener.deinit();
-    //
-    // while (true) {
-    //     const connection = try listener.accept();
-    //
-    //     try stdout.writeAll("accepted new connection");
-    //     connection.stream.close();
-    // }
+    const address = try net.Address.resolveIp("127.0.0.1", 6379);
+
+    var listener = try address.listen(.{
+        .reuse_address = true,
+    });
+    defer listener.deinit();
+
+    while (true) {
+        const connection = try listener.accept();
+
+        try stdout.writeAll("accepted new connection");
+        connection.stream.close();
+    }
 }
