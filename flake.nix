6a9bc514c1848018edd814b8af4160fe97392da9
Josh Trujillo <josh.trujillo0@pm.me>
2026-02-27 21:14:35 -0700

    added flake and direnv config.

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Flake for codecrafters redis project in zig";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, ... }@inputs:
+
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+            };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShellNoCC {
+            packages = with pkgs; [
+              zig
+              zls
+              lldb
+              codecrafters-cli
+            ];
+          };
+        }
+      );
+    };
+}
