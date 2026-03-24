cccbe1f480291223700202470e8fdf0c2eb61b8a
Josh Trujillo <josh.trujillo0@pm.me>
2026-03-23 21:58:26 -0600

    codecrafters submit [skip ci]

--- src/protocol.zig
+++ src/protocol.zig
@@ -91,7 +91,7 @@ pub fn handleCommand(alloc: std.mem.Allocator, store: *storage.Store, value: Res
             const w = &a.writer;
             try w.print("*{d}\r\n", .{range.len});
             for (range) |item| {
-                try w.print("${d}\r\n{s}\r\n", .{ item.bulk_string.len, item.bulk_string });
+                try w.print("${d}\r\n{s}\r\n", .{ item.len, item });
             }
             return a.toOwnedSlice();
         },
